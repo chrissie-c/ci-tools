7be7af5c47778b38991b9ef2fa5eeee82df2d283
Fabio M. Di Nitto <fdinitto@redhat.com>
2026-03-16 06:06:36 +0100

    Move builder updates to pipelines
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

---
+++ pipelines/libs/global/vars/updateOSNode.groovy
@@ -0,0 +1,33 @@
+// Update BaseOS on one node
+//
+// This needs to be in the global lib so it can be run from
+// a parallel Map
+def call(String agentName, Map info)
+{
+    println("Running updateOSNode on ${agentName}")
+
+    node("${agentName}") {
+	try {
+	    info['stages_run']++;
+	    // special case freebsd devel that needs ansible from built-in node
+	    if (agentName == 'built-in' && info['packager'] == 'freebsd') {
+		sh '''
+		 cd $HOME/ci-tools/bsd-update
+		 ./run-update -d
+		'''
+	    } else {
+		sh """
+		 $HOME/ci-tools/ci-wrap ci-update-${info['packager']}
+		"""
+	    }
+	}
+	// Catch any exceptions and record them
+	catch (e) {
+	    info['stages_fail'] += 1
+	    info['stages_fail_nodes'] += "${agentName} "
+	    catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
+		shNoTrace("exit 1", "Marking this stage as a failure")
+	    }
+	}
+    }
+}
