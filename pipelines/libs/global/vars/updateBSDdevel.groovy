5d9da025feb787cab123a1aa23c312dca11edc62
Fabio M. Di Nitto <fdinitto@redhat.com>
2026-03-16 09:07:14 +0100

    Move update-freebsd-devel to pipeline
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

---
+++ pipelines/libs/global/vars/updateBSDdevel.groovy
@@ -0,0 +1,29 @@
+// Update BaseOS on one node
+//
+// This needs to be in the global lib so it can be run from
+// a parallel Map
+def call(String agentName, Map info)
+{
+    println("Running updateBSDdevel on ${agentName}")
+
+    node("${agentName}") {
+	try {
+	    info['stages_run']++;
+
+	    def localinfo = getNodeProperties(agentName)
+	    def exports = getShellVariables(localinfo)
+
+	    sh """
+	     ${exports} $HOME/ci-tools/ci-wrap bsd-update/pre-update
+	    """
+	}
+	// Catch any exceptions and record them
+	catch (e) {
+	    info['stages_fail'] += 1
+	    info['stages_fail_nodes'] += "${agentName} "
+	    catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
+		shNoTrace("exit 1", "Marking this stage as a failure")
+	    }
+	}
+    }
+}
